eeaed7fd0c2de422087a31a1ad1a7a2876a21904
Vincenzo Cafasso <103500666+vincaf@users.noreply.github.com>
2022-11-15 15:16:29 +0100

    feat: add price calculation with possible discount

--- BigliettoTreno/src/org/train/price/CalcoloPrezzoBiglietto.java
+++ BigliettoTreno/src/org/train/price/CalcoloPrezzoBiglietto.java
@@ -1,5 +1,7 @@
 package org.train.price;
 
+import java.util.Scanner;
+
 public class CalcoloPrezzoBiglietto {
 	
 	/*
@@ -17,7 +19,37 @@ public class CalcoloPrezzoBiglietto {
 	*/
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Inserisci i km da percorrere: ");
+		int userKm = sc.nextInt();
+		
+		System.out.print("Inserisci la tua età: ");
+		int userAge = sc.nextInt();
+		
+		sc.close();
+		
+		double userTicket = 0.21 * userKm;
+		
+		if (userAge < 18) {
+		    userTicket = (userTicket - userTicket * 20 / 100);
+		    
+		    if(userAge < 12) {
+		    	userTicket = 0;
+				System.out.println("Biglietto gratuito per i minori di 12 anni");
+		    } else {
+		    	System.out.println("Prezzo biglietto con sconto 20% per minorenni: " + String.format("%.2f", userTicket)+"€");
+		    }
+		    
+		} else if (userAge > 65) {
+		    userTicket = (userTicket - userTicket * 40 / 100 );
+		    System.out.println("Prezzo biglietto con sconto 40% per over 65: " + String.format("%.2f", userTicket)+"€");
+		} else if (userAge < 12) {
+			userTicket = 0;
+			System.out.println("Biglietto gratuito per i minori di 12 anni");
+		} else {
+			System.out.println("Prezzo biglietto intero: " + String.format("%.2f",userTicket)+"€");
+		}
 
 	}
 
