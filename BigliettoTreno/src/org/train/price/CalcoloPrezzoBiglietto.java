f6d83888f2caadfcc1b8d752542dd45ea3d59ba1
Vincenzo Cafasso <103500666+vincaf@users.noreply.github.com>
2022-11-15 14:41:52 +0100

    create java project

---
+++ BigliettoTreno/src/org/train/price/CalcoloPrezzoBiglietto.java
@@ -0,0 +1,24 @@
+package org.train.price;
+
+public class CalcoloPrezzoBiglietto {
+	
+	/*
+	Esercitazione di oggi: Biglietto del treno
+	Nome repo: java-biglietto-treno
+	Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero. Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+	il prezzo del biglietto è definito in base ai km (0.21 € al km)
+	va applicato uno sconto del 20% per i minorenni
+	va applicato uno sconto del 40% per gli over 65
+
+	-------------------------------------
+
+	BONUS1: i minori di 12 anni viaggiano gratis
+	BONUS2: ogni km costa 1 centesimo piu' del precedente (es: 1km = 0.21E, 2km = 0.43, 3km = 0.66E, ecc)
+	*/
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
